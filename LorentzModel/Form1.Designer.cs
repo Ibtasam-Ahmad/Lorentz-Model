cc49728bae3cd18ebade2b4ab0041522cb84fbd0
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2023-09-06 10:48:36 +0500

    Add files via upload

---
+++ LorentzModel/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LorentzModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lorentzModelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerMethodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.r10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.r15ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.betweenXAndZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.betweenYAndZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.r10ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.r15ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lorentzModelToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1452, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lorentzModelToolStripMenuItem
+            // 
+            this.lorentzModelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eulerMethodToolStripMenuItem,
+            this.r10ToolStripMenuItem,
+            this.r15ToolStripMenuItem,
+            this.betweenXAndZToolStripMenuItem,
+            this.betweenYAndZToolStripMenuItem,
+            this.r10ToolStripMenuItem1,
+            this.r15ToolStripMenuItem1});
+            this.lorentzModelToolStripMenuItem.Name = "lorentzModelToolStripMenuItem";
+            this.lorentzModelToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.lorentzModelToolStripMenuItem.Text = "LorentzModel";
+            // 
+            // eulerMethodToolStripMenuItem
+            // 
+            this.eulerMethodToolStripMenuItem.Name = "eulerMethodToolStripMenuItem";
+            this.eulerMethodToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.eulerMethodToolStripMenuItem.Text = "z-versus-time r=5";
+            this.eulerMethodToolStripMenuItem.Click += new System.EventHandler(this.eulerMethodToolStripMenuItem_Click);
+            // 
+            // r10ToolStripMenuItem
+            // 
+            this.r10ToolStripMenuItem.Name = "r10ToolStripMenuItem";
+            this.r10ToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.r10ToolStripMenuItem.Text = "r=10";
+            this.r10ToolStripMenuItem.Click += new System.EventHandler(this.r10ToolStripMenuItem_Click);
+            // 
+            // r15ToolStripMenuItem
+            // 
+            this.r15ToolStripMenuItem.Name = "r15ToolStripMenuItem";
+            this.r15ToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.r15ToolStripMenuItem.Text = "r=15";
+            this.r15ToolStripMenuItem.Click += new System.EventHandler(this.r15ToolStripMenuItem_Click);
+            // 
+            // betweenXAndZToolStripMenuItem
+            // 
+            this.betweenXAndZToolStripMenuItem.Name = "betweenXAndZToolStripMenuItem";
+            this.betweenXAndZToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.betweenXAndZToolStripMenuItem.Text = "between X and Z";
+            this.betweenXAndZToolStripMenuItem.Click += new System.EventHandler(this.betweenXAndZToolStripMenuItem_Click);
+            // 
+            // betweenYAndZToolStripMenuItem
+            // 
+            this.betweenYAndZToolStripMenuItem.Name = "betweenYAndZToolStripMenuItem";
+            this.betweenYAndZToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.betweenYAndZToolStripMenuItem.Text = "between y and Z";
+            this.betweenYAndZToolStripMenuItem.Click += new System.EventHandler(this.betweenYAndZToolStripMenuItem_Click);
+            // 
+            // r10ToolStripMenuItem1
+            // 
+            this.r10ToolStripMenuItem1.Name = "r10ToolStripMenuItem1";
+            this.r10ToolStripMenuItem1.Size = new System.Drawing.Size(167, 22);
+            this.r10ToolStripMenuItem1.Text = "r=10";
+            this.r10ToolStripMenuItem1.Click += new System.EventHandler(this.r10ToolStripMenuItem1_Click);
+            // 
+            // r15ToolStripMenuItem1
+            // 
+            this.r15ToolStripMenuItem1.Name = "r15ToolStripMenuItem1";
+            this.r15ToolStripMenuItem1.Size = new System.Drawing.Size(167, 22);
+            this.r15ToolStripMenuItem1.Text = "r=15";
+            this.r15ToolStripMenuItem1.Click += new System.EventHandler(this.r15ToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1452, 849);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lorentzModelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eulerMethodToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem r10ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem r15ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem betweenXAndZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem betweenYAndZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem r10ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem r15ToolStripMenuItem1;
+    }
+}
+
---
+++ LorentzModel/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LorentzModel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void eulerMethodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = 1400;
+            double[]x = new double[size];
+            double[]y = new double[size];
+            double[]z = new double[size];
+            double[]t = new double [size];
+            double r = 5.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size-1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size-1; i++)
+            {
+                gg.FillEllipse(sb, 300 + (float)t[i]*15, 200 - (float)z[i]*15, 3, 3);
+            }
+
+
+        }
+
+        private void r10ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int size = 1400;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 10.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 70 + (float)t[i] * 15, 250 - (float)z[i] * 15, 3, 3);
+            }
+        }
+
+        private void r15ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int size = 1400;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 15.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 600+ (float)t[i] * 15, 300 - (float)z[i] * 15, 3, 3);
+            }
+        }
+
+        private void betweenXAndZToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int size = 1400;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 5.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 100 + (float)x[i] * 15, 400 - (float)z[i] * 15, 3, 3);
+            }
+        }
+
+        private void betweenYAndZToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int size = 1400;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 5.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 300 + (float)y[i] * 15, 400 - (float)z[i] * 15, 3, 3);
+            }
+        }
+
+        private void r10ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            int size = 1400;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 10.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 500 + (float)x[i] * 15, 400- (float)z[i] * 15, 3, 3);
+            }
+        }
+
+        private void r15ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            int size = 2000;
+            double[] x = new double[size];
+            double[] y = new double[size];
+            double[] z = new double[size];
+            double[] t = new double[size];
+            double r = 15.0, sigma = 10.0, b = 8.0 / 3.0, dt = 0.01;
+            x[0] = 1;
+            y[0] = 1;
+            z[0] = 1;
+            for (int i = 0; i < size - 1; i++)
+            {
+                x[i + 1] = x[i] + (sigma * (y[i] - x[i])) * dt;
+                y[i + 1] = y[i] - (x[i] * z[i] - r * x[i] + y[i]) * dt;
+                z[i + 1] = z[i] + (x[i] * y[i] - b * z[i]) * dt;
+                t[i + 1] = t[i] + dt;
+            }
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 600 + (float)y[i] * 15, 800- (float)z[i] * 15, 3, 3);
+            }
+        }
+    }
+}
